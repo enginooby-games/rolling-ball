ccf990113e8b0d43ed50e248349a72e877db77fd
Hieu Ngo <justacpulover@gmail.com>
2021-01-19 07:04:04 +0100

    Rotate mesh node of the ball

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,17 +2,22 @@ extends KinematicBody
 
 var vel = Vector3(0, 0, 0)
 const SPEED = 5
+const ROTATE_SPEED = 6
+
 
 func _ready():
 	pass
+
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_left") and Input.is_action_pressed("ui_right"):
 		vel.x = 0
 	elif Input.is_action_pressed("ui_left"):
 		vel.x = -SPEED
+		$MeshInstance.rotate_z(deg2rad(-ROTATE_SPEED))
 	elif Input.is_action_pressed("ui_right"):
 		vel.x = SPEED
+		$MeshInstance.rotate_z(deg2rad(ROTATE_SPEED))
 	else:
 		vel.x = lerp(vel.x, 0, 0.1)
 
@@ -20,8 +25,10 @@ func _physics_process(delta):
 		vel.z = 0
 	elif Input.is_action_pressed("ui_up"):
 		vel.z = -SPEED
+		$MeshInstance.rotate_x(deg2rad(-ROTATE_SPEED))
 	elif Input.is_action_pressed("ui_down"):
 		vel.z = SPEED
+		$MeshInstance.rotate_x(deg2rad(ROTATE_SPEED))
 	else:
 		vel.z = lerp(vel.z, 0, 0.1)
 
